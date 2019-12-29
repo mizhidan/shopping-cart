f9d5dc9bce4ac9b873f717ff817b67665de120b6
mizhidan <dj9824@126.com>
2019-12-29 20:02:02 +0800

    [danjie] feat: complete calcItemSum() & add eventlistener of itemlist

--- static/script/index.js
+++ static/script/index.js
@@ -48,31 +48,68 @@ var itemList = document.getElementById("item-list");
 function shoppingCart() {
   loadItemInfo(cartProducts);
   calcSum();
-  
+  itemList.addEventListener("click", function (event) {
+    var target = event.target;
+    var tdIndex = target.parentNode;
+    switch(target.className) {
+      case ("add-item"):
+        addProduct(tdIndex);
+        calcItemSum(tdIndex); 
+        calcSum();
+        break;
+      case ("reduce-item"):
+        reduceProduct(tdIndex);
+        calcItemSum(tdIndex);
+        calculateSum();
+        break;
+      case ("choose"):
+        judgeChosenState();
+        calculateSum();
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 function loadItemInfo(items) {
   for(let itemIndex = 0; itemIndex < items.length; itemIndex++) {
     var itemRow = document.createElement("tr");
+    var isChecked = items[itemIndex].checked ? "checked" : "";
     var productInTable = `
       <td>
-        <input class="choose" type="checkbox" name="ischoosed" ${items.checked} />
+        <input class="choose" type="checkbox" name="ischoosed" ${isChecked} />
       </td>
       <td>
         ${items[itemIndex].name}
       </td>
       <td>
-        ${items[itemIndex].price}
+        <span name="item-price">${items[itemIndex].price}</span>
       </td>
       <td>
-        ${items[itemIndex].count}
+        <button class="reduce-count">-</button><span name="item-count">${items[itemIndex].count}</span><button class="add-count">+</button>
       </td>
       <td>
-        ${items[itemIndex].price * items[itemIndex].count}
+        <span name="item-sum">${items[itemIndex].price * items[itemIndex].count}</span>
       </td>
     `
     itemRow.innerHTML = productInTable;
     itemList.appendChild(itemRow);
+  }
+}
+
+var itemSum = document.getElementsByName("item-sum");
+var itemCount = document.getElementsByName("item-name");
+var itemPrice = document.getElementsByName("item-price");
+
+function calcItemSum() {
+  var trIndex = tdIndex.parentNode;
+  var index = trIndex.rowIndex - 1;
+  if (productCount[index]) {
+    var sumPrice = itemSum[index];
+    var count = itemCount[index].innerText;
+    var price = itemPrice[index].innerText;
+    sumPrice.innerText = count * price;
   }
 }
 
